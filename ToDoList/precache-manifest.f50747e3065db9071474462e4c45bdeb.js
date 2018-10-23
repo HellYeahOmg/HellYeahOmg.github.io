03f673dd2f72f08c83b22d6bc3ababa1f37ec811
Vadim <vadim7552@yandex.ru>
2018-10-23 11:21:28 +0300

    Loaded ToDoList

---
+++ ToDoList/precache-manifest.f50747e3065db9071474462e4c45bdeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "962392a3d302ed3d329b",
+    "url": "/static/js/main.962392a3.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "962392a3d302ed3d329b",
+    "url": "/static/css/main.9a7ee53e.chunk.css"
+  },
+  {
+    "revision": "0e6610f1f519acb967350498682d125c",
+    "url": "/index.html"
+  }
+];
